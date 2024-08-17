ab9c547cf9197b923b156f73d9ecdf52cecc24f2
cecillk <klufiocecil99@gmail.com>
2024-08-17 20:17:06 +0000

    Configured Procfile and Gunicorn for Render deployment

--- Learnbuddy/settings.py
+++ Learnbuddy/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for Learnbuddy project.
 
@@ -23,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$d0qiec!x5d)c770p=&$qkt9t92-k9v#3c=cg1%e%tz#=&zb)1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
